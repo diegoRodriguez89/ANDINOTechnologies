24b5dae6df7072795793ed2f2c58ff7db0ae1c4b
NoelSopena <noel.sopena@upr.edu>
2015-04-01 20:47:16 -0400

    Create lang.es.php

---
+++ lang.es.php
@@ -0,0 +1,135 @@
+<?php   
+
+   $lang = array();
+
+  //INDEX - INICIAR_SESION
+  $lang['language'] = 'Language';
+  $lang['signIn'] = 'Iniciar Sesión'; 
+  $lang['userName'] = 'Nombre de Usuario';
+  $lang['userPassword'] = 'Contraseña';
+  $lang['signup'] = 'Iniciar';
+  $lang['userChange'] = 'Cambiar';  
+  $lang['remember'] = 'Recordar Información'; 
+  $lang['enter'] = 'Someter'; 
+  $lang['logout'] = 'Salir';
+
+  //ADMIN PAGE
+  $lang['adminPage'] = 'Página del Administrador'; 
+  $lang['searchButton'] = 'Buscar';
+  $lang['eList'] = 'Mostrar Lista de Empleados';
+  $lang['genStats'] = 'Generar Estadísticas'; 
+  $lang['case'] = 'Caso';
+  $lang['docType'] = 'Tipo de Documento';
+  $lang['dDate'] = 'Fecha de Vencimiento'; 
+  $lang['status'] = 'Estado'; 
+  $lang['viewCase'] = 'Mostrar'; 
+  
+  //EMPLOYEE LIST VIEW
+  $lang['eListHeader'] = 'Lista de Empleados'; 
+  $lang['eAdd'] = 'Añadir Empleado';
+  $lang['eName'] = 'Nombre';
+  $lang['eLastNames'] = 'Apellidos';
+  $lang['ePosition'] = 'Posición'; 
+  $lang['eEdit'] = 'Editar';
+  $lang['eDelete'] = 'Borrar'; 
+  $lang['eCancel'] = 'Cancelar'; 
+ 
+
+  //ADD_EMPLOYEE
+  $lang['eFirstName'] = 'Nombre';
+  $lang['eInitial'] = 'Inicial'; 
+  $lang['eLastName'] = 'Apellido Paterno'; 
+  $lang['eMaidenName'] = 'Apellido Materno'; 
+  $lang['eEmail'] = 'Correo Electrónico'; 
+  $lang['eIdNumber'] = 'Número de Identificación'; 
+  $lang['ePhone'] = 'Número de Teléfono'; 
+  $lang['eType'] = 'Tipo de Empleado'; 
+   $lang['eType_reservist'] = 'Reservista'; 
+    $lang['eType_colonel'] = 'Coronel'; 
+      $lang['eType_lieutenant_colonel'] = 'Teniente Coronel'; 
+        $lang['eType_commander'] = 'Comandante'; 
+          $lang['eType_inspector'] = 'Inspector';
+              $lang['eType_colonel'] = 'Coronel'; 
+                  $lang['eType_captain'] = 'Capitán'; 
+                      $lang['eType_lieutenant'] = 'Teniente'; 
+                        $lang['eType_sargent'] = 'Sargento'; 
+                          $lang['eType_agent'] = 'Agente'; 
+                            $lang['eType_cadet'] = 'Cadete'; 
+                              $lang['eType_adm_assistant'] = 'Asistente Administrativa'; 
+                                $lang['eType_adm_system'] = 'Adimistrador de Sistema I'; 
+                                  $lang['eType_adm_office_techs'] = 'Técnicas de Sistemas de Oficina III';
+  $lang['eTemporaryUser'] = 'Nombre de Usuario Temporero';
+  $lang['eTemporaryPass'] = 'Contraseña Temporera';
+  $lang['eSubmit'] = 'Añadir'; 
+
+
+
+  //EDIT_EMPLOYEE
+  $lang['eEdit'] = 'Modificar Empleado';              
+  
+  //GENERATE STATS PAGE
+  $lang['genBy'] = 'Por';
+  $lang['genSubcategory'] = 'Subcategoría'; 
+  $lang['genQuantity'] = 'Cuantía'; 
+  $lang['genAmount'] = 'Cantidad'; 
+  $lang['sDate'] = 'Fecha de Inicio'; 
+  $lang['eDate'] = 'Fecha de Vencimiento'; 
+  $lang['generate'] = 'Generar'; 
+  $lang['doc_lawsuit'] = 'Demanda'; 
+  $lang['doc_motion'] = 'Moción'; 
+  $lang['doc_subpoena'] = 'Citación'; 
+  $lang['doc_requirements'] = 'Requerimientos';
+  $lang['doc_others'] = 'Otros'; 
+
+  //SECRETARY PROFILE
+  $lang['s_profile'] = 'Perfil de la Secretaria';
+    $lang['addContract'] = 'Añadir Contrato';
+
+  //ADD_CASE 
+  $lang['add_case'] = 'Añadir Caso'; 
+  $lang['case_num'] = '# Caso';
+  $lang['date_issued'] = 'Fecha de Comunicado';
+  $lang['date_received'] = 'Fecha Recibido'; 
+  $lang['date_due'] = 'Fecha de Vencimiento'; 
+  $lang['case_apellant'] = 'Apelante'; 
+  $lang['case_receiver'] = 'Destinatario'; 
+  $lang['case_region'] = 'Región'; 
+  $lang['case_sender'] = 'Remitente'; 
+  $lang['department'] = 'Departmento'; 
+  $lang['case_comment'] = 'Comentario'; 
+  $lang['case_subject'] = 'Asunto'; 
+  $lang['case_referred'] = 'Referido a'; 
+  $lang['doc_print']     = 'Imprimir'; 
+  $lang['doc_view']      = 'Documento';
+
+  //ADD_CONTRACT 
+  $lang['contractHeader'] = 'Contrato'; 
+  $lang['contract_num'] = '# Contract'; 
+  $lang['contType'] = 'Tipo de Contrato'; 
+  $lang['serviceType'] = 'Tipo de Servicio';
+  $lang['print'] = 'Imprimir';
+
+  //CASE
+  $lang['case'] = 'Caso'; 
+  $lang['case_modify'] = 'Editar'; 
+  $lang['status'] = 'Estado'; 
+  $lang['status_processed'] = 'Procesado'; 
+  $lang['status_not_processed'] = 'No Procesado'; 
+
+  //CASES PAGE
+  $lang['case_page'] = 'Página de Casos'; 
+
+  //LAWYER PROFILE
+  $lang['l_profile']    = 'Perfil del Abogado'; 
+  $lang['l_name']       = 'Lic. Nombre'; 
+  $lang['l_docs']       = 'Mis Documentos'; 
+  $lang['allDocs']      = 'Todos los Documentos'; 
+
+  //SEARCH_PAGE
+  $lang['searchCaseHeader'] = 'Búsqueda de Casos'; 
+  $lang['search_date']      = 'Fecha'; 
+  $lang['apellant_name']    = 'Nombre del Apelante'; 
+ 
+
+
+?>
