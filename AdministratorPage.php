1e6d1c77e35a112febc36589f92adf126d888c42
NoelSopena <noel.sopena@upr.edu>
2015-04-01 20:23:30 -0400

    Create AdministratorPage.php

---
+++ AdministratorPage.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+
+  $serverName = "127.0.0.1";
+  $connectionInfo = array("Database"=>"PoliceTest", "UID"=>"sa", "PWD"=>"A06a30adr5d");
+
+  $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+  if($conn) {
+    echo "Admin Page";
+  }
+  
+?> 
