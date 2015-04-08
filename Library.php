bc77c668b4e37745630c5f858dafa4f8ad255da8
NoelSopena <noel.sopena@upr.edu>
2015-04-07 22:01:32 -0400

    Update Library.php

--- Library.php
+++ Library.php
@@ -1,5 +1,10 @@
 <?php
 
+  /*
+  This file contains the functions that define the navigational toolbar. 
+  */
+
+
 function navbar($lang){
 
 			echo"<nav class='navbar navbar-default'>";
@@ -93,6 +98,34 @@ function navbar($lang){
     		echo"	    <li><a href='?lang=es'>Español</a></li>";
    			echo"		</ul>";
 			echo"	</div>";
+		    echo"    <a class='btn btn-primary' href='IniciarSesionhtml.php'>$logout</a>";
+		    echo"  </form>";
+		    echo"</div><!-- /.navbar-collapse -->";
+		  	echo"</div><!-- /.container-fluid -->";
+			echo"</nav>";
+
+
+
+	}
+
+	function navbarLogout($logout){
+
+		  	echo" <nav class='navbar navbar-default'>";
+		  	echo"<div class='container-fluid'>";
+		    echo"<div class='navbar-header'>";
+		    echo" <!-- This part of the code is for the name of the application -->";
+		    echo"  <a class='navbar-brand'>ANDINO Legal Solution</a>";
+		    echo"  <!-- Brand and toggle get grouped for better mobile display -->";
+		    echo"  <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>";
+            echo"           <span class='sr-only'>Toggle navigation</span>";
+            echo"           <span class='icon-bar'></span>";
+            echo"           <span class='icon-bar'></span>";
+            echo"           <span class='icon-bar'></span>";
+            echo"         </button>";
+		 	echo"	    </div>";
+		    echo"<!-- Collect the nav links, forms, and other content for toggling -->";
+		    echo"<div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>";   
+		    echo"  <form class='navbar-form navbar-right'>";
 		    echo"    <a class='btn btn-primary' href='IniciarSesionhtml.php'>$logout</a>";
 		    echo"  </form>";
 		    echo"</div><!-- /.navbar-collapse -->";
