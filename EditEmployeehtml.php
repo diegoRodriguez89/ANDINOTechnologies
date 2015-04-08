bca3260e8775389168ab566709bb8c939fabd5e3
NoelSopena <noel.sopena@upr.edu>
2015-04-07 21:46:29 -0400

    Update EditEmployeehtml.php

--- EditEmployeehtml.php
+++ EditEmployeehtml.php
@@ -1,9 +1,13 @@
 <?php
   session_start();
 
-  	include_once 'common.php';
+	//Includes the libraries to change the language of the page (english/spanish) and the navigational bar
+	include_once 'common.php';
 	include 'library.php';
 
+	if($_SESSION[job] <> 'admin'){
+		die("You are not allowed in this page. :P");
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,8 +28,11 @@
 	</script>
 	
 	<body>
-		<!-- Static navbar -->
 		<?php 
+		/*
+		This function displays the information in the navigation bar. It includes the system's header, the
+		language selection dropdown and logout buttons.
+		*/
 			navbarEmployeeList($lang['language'],$lang['logout']);
 		?>
 		
@@ -39,10 +46,21 @@
 			<br></br>
 			<?php
 		    session_start();
+		    
+		    /* Server
+    		$serverName = the name of the server to connect
+    		$connectionInfo = creates an array with the database name, the user id of the database and the user's password of the database
+    		$conn = sqlsrv_connect() = is the function to connect with the server
+  			*/
 		    $serverName = "127.0.0.1";
   			$connectionInfo = array("Database"=>"PoliceTest", "UID"=>"sa", "PWD"=>"A06a30adr5d");
-
   			$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+  			/* SQL
+    		$sql = query to fetch the information of the employee in the database from the variables below
+    		$stmt = sqlsrv_query() = prepares and executes the query
+    		$row = sqlsrv_fetch_array() = returns the row as an array
+   			 */
 		    $sql = "SELECT Name, MiddleName, LastName, MaidenName, Job FROM Employee WHERE Username = '$_SESSION[username]'";
 				$stmt = sqlsrv_query($conn, $sql);
 				while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
@@ -66,7 +84,7 @@
 				<div class="form-group"> 
 					<label for="employee_name" class="col-sm-2 control-label"><?php echo $lang['eInitial']; ?>:</label> 
 					<div class="col-sm-5">
-						<!-- This input text is for the name of the employee -->
+						<!-- This input text is for the initial of the employee -->
 						<input type="text" class="form-control" id="employeeInitial" name="employeeInitial" placeholder="Inicial del empleado" onkeyup="valid(this)" onblur="valid(this)" value=<?php echo $inicial ?>>
 					</div>
 				</div>
@@ -82,7 +100,7 @@
 				<div class="form-group"> 
 					<label for="employee_lastname2" class="col-sm-2 control-label"><?php echo $lang['eMaidenName']; ?>:</label> 
 					<div class="col-sm-5">
-						<!-- This input text is for the second last name of the employee -->
+						<!-- This input text is for the maiden name of the employee -->
 						<input type="text" class="form-control" id="employeeLastname2" name="employeeLastname2" placeholder="Segundo Apellido del Empleado" onkeyup="valid(this)" onblur="valid(this)" value=<?php echo $apellido2 ?>>
 					</div> 
 				</div>
@@ -90,7 +108,7 @@
 				<div class="form-group"> 
 					<label for="Type_Employee" class="col-sm-2 control-label"><?php echo $lang['eType']; ?>:</label>
 					<div class="col-sm-5" id="TypeEmployee">
-						<!-- This dropdown is for selectinf the type of employee -->
+						<!-- This dropdown is for select the type of employee -->
 					<select class="form-control" name="TypeEmployee">
 						<option><?php print $lang['eType'] ;?>  </option>
 						<option value="Técnicas de Sistemas de Oficina III"><?php print $lang['eType_adm_office_techs'];?> </option>
@@ -113,10 +131,10 @@
 				<br></br>
 						
 				<div class="row">
-					<!-- This button is for canceling everithing and returns to the administrators page -->
-					<a class="btn btn-primary pull-right" href="employeeListhtml.php"><?php echo $lang['eCancel']; ?></a>
 					<!-- This button is for adding a employee to the system -->
-					<button class="btn btn-primary pull-right" style="margin-right: 4px" type="submit" form="editEmployee"><?php echo $lang['enter']; ?></button>
+					<button class="btn btn-primary col-md-offset-5" type="submit" form="editEmployee"><?php echo $lang['enter']; ?></button>
+					<!-- This button is for canceling everithing and returns to the administrators page -->
+					<a class="btn btn-primary" href="employeeListhtml.php"><?php echo $lang['eCancel']; ?></a>
 				</div>
 				
 			</form>
